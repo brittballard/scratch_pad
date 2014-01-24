7a9b808dd3e27a535ba3ccb2d8d4e0d7ec17accb
Britt Ballard <britt@thoughtbot.com>
2014-01-23 17:47:35 -0800

    Server updates

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,8 +3,9 @@ class Note < ActiveRecord::Base
 
   private
 
-  def method_name
+  def ensure_title_or_content
     unless title.present? || content.present?
       errors.add(:title, 'Must provide a title or content')
+    end
   end
 end
