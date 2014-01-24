3610639a5f0786a8fe54146a3a5440ebcc26139a
Britt Ballard <britt@thoughtbot.com>
2014-01-24 11:28:41 -0800

    Continue associations

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.0.1'
 
+gem 'active_model_serializers', '~> 0.8.0'
 gem 'backbone-on-rails', '~> 1.1.0.0'
 gem 'coffee-rails', '~> 4.0.0'
-gem 'jbuilder', '~> 1.2'
 gem 'jquery-rails'
 gem 'lodash-rails', '~> 2.2.1'
 gem 'momentjs-rails', '~> 2.4'
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,12 +1,5 @@
 class Note < ActiveRecord::Base
   validate :ensure_title_or_content
-  belongs_to :body, polymorphic: true
-
-  private
-
-  def ensure_title_or_content
-    unless title.present? || content.present?
-      errors.add(:title, 'Must provide a title or content')
-    end
-  end
+  validate :body, presence: true
+  belongs_to :body, polymorphic: true, autosave: true, inverse_of: :note, dependent: :destroy
 end
